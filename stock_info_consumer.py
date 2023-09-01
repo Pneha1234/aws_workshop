3e8d6957fd1e57fb05a12abed5f25e8cda166483
Prashant Karki <i35115@verisk.com>
2023-09-01 15:14:43 +0545

    changes consumer

--- stock_info_consumer.py
+++ stock_info_consumer.py
@@ -4,10 +4,23 @@ import os
 stepfunctions = boto3.client('stepfunctions')
 
 def lambda_handler(event, context):
+    stock_portfolio_str = os.environ['STOCK_PORTFOLIO'] # comma separated stock names from given list: ADBL,EBL,GBIME,HBL,KBL,MBL,NABIL,NBL,NCCB,PCBL,PRVU,SBI,SCB,SRBL,STC,API,UIC,LIC,NLIC
+    stock_portfolio_list = stock_portfolio_str.split(',')
+    for owned_stock_name in stock_portfolio_list:
+        owned_stock_name = owned_stock_name.strip().upper()
+
+    
+
     try:
         for record in event['Records']:
             # Process SQS message
             stock_detail = record['body']
+            
+            filtered_stock_detail = {}
+            for stock_name in stock_portfolio_list:
+                if stock_name in stock_detail:
+                    filtered_stock_detail[stock_name] = stock_detail[stock_name]
+
             print("Message received from SQS:", stock_detail)
 
             # Trigger Step Function with the message data as input

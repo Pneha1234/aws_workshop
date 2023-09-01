9539ab215fcc16aceca0aa0e99e7e106e10f146c
Prashant Karki <i35115@verisk.com>
2023-09-02 01:39:02 +0545

    ete fixes

--- stock_info_consumer.py
+++ stock_info_consumer.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 import os
 
@@ -7,31 +8,27 @@ def lambda_handler(event, context):
     stock_portfolio_str = os.environ['STOCK_PORTFOLIO'] # comma separated stock names from given list: ADBL,EBL,GBIME,HBL,KBL,MBL,NABIL,NBL,NCCB,PCBL,PRVU,SBI,SCB,SRBL,STC,API,UIC,LIC,NLIC
     stock_portfolio_list = stock_portfolio_str.split(',')
     for owned_stock_name in stock_portfolio_list:
-        owned_stock_name = owned_stock_name.strip().upper()
-
-    
+        owned_stock_name = owned_stock_name.strip().upper()    
 
     try:
-        filtered_stock_detail = {}
         for record in event['Records']:
             # Process SQS message
-            stock_detail = record['body']
+            current_stock_detail = record['body']
+            current_stock_name = next(iter(json.loads(current_stock_detail)))
             
-            for stock_name in stock_portfolio_list:
-                if stock_name not in stock_detail:
-                    return {
-                    'statusCode': 200,
-                    'body': 'stock not owned'
-                }
-                filtered_stock_detail[stock_name] = stock_detail[stock_name]
+            if current_stock_name not in stock_portfolio_list:
+                return {
+                'statusCode': 200,
+                'body': 'stock not owned'
+            }
 
-            print("Message received from SQS:", stock_detail)
+            print("Message received from SQS:", current_stock_detail)
 
         # Trigger Step Function with the message data as input
         state_machine_arn = os.environ['STATE_MACHINE_ARN']
         response = stepfunctions.start_execution(
             stateMachineArn=state_machine_arn,
-            input=filtered_stock_detail
+            input=current_stock_detail
         )
         print("Step Function execution triggered:", response)
 

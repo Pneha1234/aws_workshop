93f144a4af6ad48b814265bc07e4f05eb8d46b30
Prashant Karki <i35115@verisk.com>
2023-09-04 11:32:38 +0545

    missed code

--- generate_stock_recommendation.py
+++ generate_stock_recommendation.py
@@ -2,12 +2,9 @@ import os
 
 def lambda_handler(event, context):
     try:
-
         stock_live_details = event
         print("Input data from Step Functions:", stock_live_details)
-
         recommendation = generate_stock_recommendation(stock_live_details)
-
         return {
             'statusCode': 200,
             'body': recommendation
@@ -26,11 +23,9 @@ def lambda_handler(event, context):
 def generate_stock_recommendation(stock_live_details):
     current_stock_name = next(iter(stock_live_details))
     current_stock_price = stock_live_details[current_stock_name]
-
     if current_stock_price > 650:
         return ({"status":"sell", "stock_name":current_stock_name, "rate": current_stock_price})
     elif current_stock_price < 150:
         return ({"status":"buy", "stock_name":current_stock_name, "rate": current_stock_price})
     else:
         return ({"status":"hold", "stock_name":current_stock_name, "rate": current_stock_price})
-        
